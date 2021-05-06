4ddf1951c6e182679df23fc976a56fe1149b71f5
LianeHughes <68756672+LianeHughes@users.noreply.github.com>
2021-05-06 16:31:20 +0200

    Initiate repository folder and SLLextfund script add
    start of the global analysis files and first donut/pie chart script for funding

---
+++ Infra_global_plots/Total_SLLandext_funding.py
@@ -0,0 +1,123 @@
+"""This script will generate a pie chart (like figure 6 in IEC report)"""
+
+import pandas as pd
+import plotly.graph_objects as go
+import os
+from colour_science_2020 import (
+    SCILIFE_COLOURS_NOGREY,
+)
+
+Facility_data = pd.read_excel(
+    "Data/Single data 2020_ver3.xlsx",
+    sheet_name="Single Data",
+    header=0,
+    engine="openpyxl",
+    keep_default_na=False,
+)
+
+fund_data = pd.read_excel(
+    "Data/Other funding 2020.xlsx",
+    sheet_name="Sheet1",
+    header=0,
+    engine="openpyxl",
+    keep_default_na=False,
+)
+
+Facility_data.rename(
+    columns={
+        "Funding 2020 SciLifeLab (kSEK)": "Amount (kSEK)",
+    },
+    inplace=True,
+)
+
+SLL_funding = Facility_data[["Facility", "Platform", "Amount (kSEK)"]]
+SLL_funding.insert(loc=2, column="Financier", value="SciLifeLab")
+
+Funding_comb = pd.concat([SLL_funding, fund_data])
+
+
+Funding_comb["Group_finance"] = Funding_comb["Financier"]
+
+
+Funding_comb["Group_finance"] = Funding_comb["Group_finance"].replace(
+    dict.fromkeys(
+        [
+            "Universities",
+            "UU",
+            "Chalmers",
+            "LiU",
+            "SLU",
+            "LU",
+            "UmU",
+            "KTH",
+            "SU",
+            "GU",
+            "KI",
+            "ÖRU",
+        ],
+        "University",
+    ),
+    regex=True,
+)
+
+Funding_comb["Group_finance"] = Funding_comb["Group_finance"].replace(
+    dict.fromkeys(
+        ["University hospital", "County council", "ALF"],
+        "Healthcare",
+    ),
+    regex=True,
+)
+
+Funding_comb["Group_finance"] = Funding_comb["Group_finance"].replace(
+    dict.fromkeys(
+        ["Elixir", "Nordforsk", "SSF", "Vinnova"],
+        "Other",
+    ),
+    regex=True,
+)
+
+
+colours = [
+    SCILIFE_COLOURS_NOGREY[2],
+    SCILIFE_COLOURS_NOGREY[14],
+    SCILIFE_COLOURS_NOGREY[4],
+    SCILIFE_COLOURS_NOGREY[0],
+    SCILIFE_COLOURS_NOGREY[12],
+    SCILIFE_COLOURS_NOGREY[8],
+]
+
+Funding_comb_group = Funding_comb.groupby(["Group_finance"]).sum().reset_index()
+Funding_comb_group["Funding_MSEK"] = (
+    (Funding_comb_group["Amount (kSEK)"] / 1000).round().astype(int)
+)
+print(Funding_comb_group)
+
+
+fig = go.Figure(
+    go.Pie(
+        values=Funding_comb_group["Funding_MSEK"],
+        labels=Funding_comb_group["Group_finance"],
+        hole=0.6,
+        marker=dict(colors=colours, line=dict(color="#000000", width=1)),
+        direction="clockwise",
+        sort=True,
+    )
+)
+
+fig.update_traces(
+    textposition="outside",
+    texttemplate="%{label} <br>(%{value})",
+)
+fig.update_layout(
+    margin=dict(l=100, r=100, b=100, t=100),
+    font=dict(size=34),
+    showlegend=False,
+    width=1000,
+    height=1000,
+    autosize=False,
+)
+if not os.path.isdir("Plots/Fund_Pies"):
+    os.mkdir("Plots/Fund_Pies")
+# fig.show()
+
+fig.write_image("Plots/Fund_Pies/total_fund_SLLandext.pdf", scale=3)
